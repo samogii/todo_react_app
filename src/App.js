7d00d348f005dd0cbdbc4da1d8257addca0694e0
samogii <sami20021381@gmail.com>
2024-05-12 14:46:22 +0330

    updating

--- src/App.js
+++ src/App.js
@@ -8,7 +8,6 @@ import {
 	Group,
 	Card,
 	ActionIcon,
-	Code,
 } from '@mantine/core';
 import { useState, useRef, useEffect } from 'react';
 import { MoonStars, Sun, Trash } from 'tabler-icons-react';
@@ -16,16 +15,15 @@ import { MoonStars, Sun, Trash } from 'tabler-icons-react';
 import {
 	MantineProvider,
 	ColorSchemeProvider,
-	ColorScheme,
 } from '@mantine/core';
-import { useColorScheme } from '@mantine/hooks';
+// import { useColorScheme } from '@mantine/hooks';
 import { useHotkeys, useLocalStorage } from '@mantine/hooks';
 
 export default function App() {
 	const [tasks, setTasks] = useState([]);
 	const [opened, setOpened] = useState(false);
 
-	const preferredColorScheme = useColorScheme();
+	// const preferredColorScheme = useColorScheme();
 	const [colorScheme, setColorScheme] = useLocalStorage({
 		key: 'mantine-color-scheme',
 		defaultValue: 'light',
